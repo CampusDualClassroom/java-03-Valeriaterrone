236012ce5550cec4803074ee062dbb0512516918
valeriaterrone <valeriaprogramacioncurso@gmail.com>
2025-01-14 13:54:50 +0100

    Ejercicio 3 listo! gracias

--- src/main/java/com/campusdual/classroom/Exercise03.java
+++ src/main/java/com/campusdual/classroom/Exercise03.java
@@ -11,24 +11,24 @@ public class Exercise03 {
     // Esto es una funcion, ya que devuelve un valor
     // ↓: Tiene que devolver el siguiente mensaje: "Ups. No pudimos validar tus datos. Tu nombre de usuario no es " y la cadena de texto que se le pasa por parámetro
     public static String error(String name) {
-        return "Upss. No pudimos validar tus datos. Tu nombre de usuario es: " + name;
+        return "Ups. No pudimos validar tus datos. Tu nombre de usuario no es " + name;
     }
 
     //Esto es un procedimiento, ya que no devuelve un valor
     // ↓: Tiene que comprobar si el usuario que recibe por parámetro es "Sebas" y su contraseña es "sebas01". Si es correcto, que muestre el mensaje de la función greetings() y si no que muestre el mensaje de la función error()
     public static void checkUser(String user, String pass) {
-    String validUser = "Valeria";
-    String validPass = "valeria03";
+    String correctUser = "Sebas";
+    String correctPass = "sebas01";
 
-    if (validUser.equals(user) && validPass.equals(pass)) {
-        System.out.println(greetings(user));
+    if (correctUser.equals(user) && correctPass.equals(pass)) {
+        System.out.println(greetings(correctUser));
     } else {
-        System.out.println(error(validUser));
+        System.out.println(error(correctUser));
      }
     }
 
     public static void main(String[] args) {
-       checkUser("Valeria", "valeria03");
+       checkUser("Sebas", "sebas01");
        checkUser("Anderes", "penelope");
     }
 }
